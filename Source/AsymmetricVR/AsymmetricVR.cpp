662bcbf44663f557545e8ba93f5168bb1790b702
Luna D <luna@nighty.cloud>
2024-06-18 23:00:30 +0200

    initial commit

---
+++ Source/AsymmetricVR/AsymmetricVR.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AsymmetricVR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AsymmetricVR, "AsymmetricVR" );
---
+++ Source/AsymmetricVR/Public/AsymmetricVrGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AsymmetricVrGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASYMMETRICVR_API AAsymmetricVrGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
